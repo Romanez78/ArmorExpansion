7f6cf2a119cb1f7d382c910f1d1f818e99c9e22f
szapp <dev.szapp@gmail.com>
2024-09-12 08:06:02 +0200

    Update initialization and file names

--- Ninja/ArmorExpansion/Content/Init.d
+++ Ninja/ArmorExpansion/Content/Init.d
@@ -4,8 +4,8 @@ const string Patch_AE_Version = "ArmorExpansion 1.0";
  * Initialization function called by Ninja after "Init_Global" (G2) / "Init_<Levelname>" (G1)
  */
 func void Patch_AE_Init() {
-    // Init Ikarus&Lego
-    LeGo_MergeFlags(LeGo_PermMem);
+    // Init Ikarus
+    MEM_InitAll();
 
     MEM_Info(ConcatStrings(ConcatStrings("Initialize ", Patch_AE_Version), "."));
     Patch_AE_Init();
