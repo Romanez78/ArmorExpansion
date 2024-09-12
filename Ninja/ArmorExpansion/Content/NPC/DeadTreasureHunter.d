b4b99f2a4f49cadb9c405efad34c56ed174d85c7
szapp <dev.szapp@gmail.com>
2024-09-12 15:01:37 +0200

    Adjust the NPC ID
    It should not be an ID that might be already used in a mod

--- Ninja/ArmorExpansion/Content/NPC/DeadTreasureHunter.d
+++ Ninja/ArmorExpansion/Content/NPC/DeadTreasureHunter.d
@@ -1,22 +1,22 @@
 
-instance BAU_989_DeadTreasureHunter (Npc_Default)
+instance Patch_AE_DeadTreasureHunter (Npc_Default)
 {
 	// ------ NSC ------
 	name 		= "Mrtvý lovec pokladů";
 	guild 		= GIL_BAU;
-	id 			= 989;
+	id 			= 44989;  // An ID that is unlikely already used in a mod
 	voice 		= 14;
 	flags       = 0;		//NPC_FLAG_IMMORTAL oder 0
 	npctype		= NPCTYPE_AMBIENT;
-	
+
 	// ------ Attribute ------
 	B_SetAttributesToChapter (self, 2);																	//setzt Attribute und LEVEL entsprechend dem angegebenen Kapitel (1-6)
-	
+
 	// ------ Kampf-Taktik ------
 	fight_tactic		= FAI_HUMAN_STRONG;	// MASTER / STRONG / COWARD
-	
+
 	// ------ Equippte Waffen ------																	//Munition wird automatisch generiert, darf aber angegeben werden
-	
+
 	// ------ Inventory ------
 	CreateInvItems (self, ITWR_BluePrint_StewarkArmor,1);
 	CreateInvItems (self, ITMI_Gold,250);
@@ -34,23 +34,23 @@ instance BAU_989_DeadTreasureHunter (Npc_Default)
 	CreateInvItems (self, ItMi_Nugget,1);
 	CreateInvItems (self, ItMw_Rubinklinge,1);
 	// ------ visuals ------																			//Muss NACH Attributen kommen, weil in B_SetNpcVisual die Breite abh. v. STR skaliert wird
-	B_SetNpcVisual 		(self, MALE, "Hum_Head_FatBald", Face_N_OldBald_Jeremiah, BodyTex_N, ITAR_djg_crawler);	
+	B_SetNpcVisual 		(self, MALE, "Hum_Head_FatBald", Face_N_OldBald_Jeremiah, BodyTex_N, ITAR_djg_crawler);
 	Mdl_SetModelFatness	(self, 2);
 	Mdl_ApplyOverlayMds	(self, "Humans_Relaxed.mds"); // Tired / Militia / Mage / Arrogance / Relaxed
 
 	// ------ NSC-relevante Talente vergeben ------
 	B_GiveNpcTalents (self);
-	
+
 	// ------ Kampf-Talente ------																		//Der enthaltene B_AddFightSkill setzt Talent-Ani abhängig von TrefferChance% - alle Kampftalente werden gleichhoch gesetzt
 	B_SetFightSkills (self, 60); //Grenzen für Talent-Level liegen bei 30 und 60i
 
 	// ------ TA anmelden ------
-	daily_routine 		= Rtn_Start_989;
+	daily_routine 		= Rtn_Start_44989;
 };
 
-FUNC VOID Rtn_Start_989 ()
+FUNC VOID Rtn_Start_44989 ()
 {
     TA_Stand_WP	(08,00,23,00,"DT_E2_09");
-    TA_Stand_WP	(23,00,08,00,"DT_E2_09");			
+    TA_Stand_WP	(23,00,08,00,"DT_E2_09");
 };
 
--- Ninja/ArmorExpansion/Content/Startup.d
+++ Ninja/ArmorExpansion/Content/Startup.d
@@ -6,5 +6,6 @@ func void Patch_AE_Startup_Demontower()
     // Continue an execution of original function
     ContinueCall();
 
-    Wld_InsertNpc(BAU_989_DeadTreasureHunter, "DT_E2_09"); B_KillNpc(BAU_989_DeadTreasureHunter);
+    Wld_InsertNpc(Patch_AE_DeadTreasureHunter, "DT_E2_09");
+    B_KillNpc(Patch_AE_DeadTreasureHunter);
 };
