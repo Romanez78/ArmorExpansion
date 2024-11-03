61ced9ef8bc4e01c5fca820ed8c82e071c30119f
Romanez78 <85283033+Romanez78@users.noreply.github.com>
2024-11-03 15:47:51 +0100

    Update IT_Belts.d
    Leather belt script added

--- Ninja/ArmorExpansion/Content/Items/IT_Belts.d
+++ Ninja/ArmorExpansion/Content/Items/IT_Belts.d
@@ -63,3 +63,36 @@ func void Patch_AE_UnEquip_ItBE_Addon_SLD_01()
 
     ContinueCall();
 };
+
+func void Patch_AE_Equip_ItBE_Addon_Leather_01()
+{
+    if (true == Npc_IsPlayer(self))
+    {
+        if (true == Patch_AE__ITAR_GAMBESON_Equipped)
+        {
+            self.protection[PROT_EDGE] += BA_Bonus01;
+            self.protection[PROT_BLUNT] += BA_Bonus01;
+            self.protection[PROT_POINT] += BA_Bonus01;
+            self.protection[PROT_MAGIC] += BA_Bonus01;
+            self.protection[PROT_FIRE] += BA_Bonus01;
+        };
+    };
+
+    ContinueCall();
+};
+func void Patch_AE_UnEquip_ItBE_Addon_Leather_01()
+{
+    if (true == Npc_IsPlayer(self))
+    {
+        if (true == Patch_AE__ITAR_GAMBESON_Equipped)
+        {
+            self.protection[PROT_EDGE] -= BA_Bonus01;
+            self.protection[PROT_BLUNT] -= BA_Bonus01;
+            self.protection[PROT_POINT] -= BA_Bonus01;
+            self.protection[PROT_MAGIC] -= BA_Bonus01;
+            self.protection[PROT_FIRE] -= BA_Bonus01;
+        };
+    };
+
+    ContinueCall();
+};
