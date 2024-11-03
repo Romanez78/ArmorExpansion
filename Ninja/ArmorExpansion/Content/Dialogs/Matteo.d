3af5b94e7ff4578337048ab1e6127eab84ade5bf
Romanez78 <85283033+Romanez78@users.noreply.github.com>
2024-11-03 15:08:51 +0100

    Update Matteo.d
    Added dialogue for 2 new armors

--- Ninja/ArmorExpansion/Content/Dialogs/Matteo.d
+++ Ninja/ArmorExpansion/Content/Dialogs/Matteo.d
@@ -34,3 +34,78 @@ func void Patch_AE_DIA_Matteo_GAMBESON_Info()
         AI_Output(self, other, PATCH_AE_DIA_MATTEO_GAMBESON_09_02);
     };
 };
+
+//-------------------------------------
+var int Patch_AE_Matteo_HUNTERJACKETBought;
+//-------------------------------------
+
+instance Patch_AE_DIA_Matteo_HUNTERJACKET (C_INFO)
+{
+    npc          =  VLK_416_Matteo;
+    nr           =  851;
+    condition    =  Patch_AE_DIA_Matteo_HUNTERJACKET_Condition;
+    information  =  Patch_AE_DIA_Matteo_HUNTERJACKET_Info;
+    permanent    =  TRUE;
+    description  =  Patch_AE_DIA_Matteo_HUNTERJACKET_desc;
+};
+func int Patch_AE_DIA_Matteo_HUNTERJACKET_Condition()
+{
+    if (Npc_KnowsInfo(other, DIA_Matteo_SellWhat))
+    && (Patch_AE_Matteo_HUNTERJACKETBought == FALSE)
+    {
+        return TRUE;
+    };
+};
+func void Patch_AE_DIA_Matteo_HUNTERJACKET_Info()
+{
+    AI_Output(other, self, PATCH_AE_DIA_MATTEO_HUNTERJACKET_15_00);
+
+    if (B_GiveInvItems(other, self, ItMi_Gold, 800))
+    {
+        AI_Output(self, other, PATCH_AE_DIA_MATTEO_HUNTERJACKET_09_01);
+
+        Patch_AE_Matteo_HUNTERJACKETBought = B_GiveInvItems(self, other, ItAr_HUNTERJACKET_ARMOREXPANSION, 1);
+    }
+    else
+    {
+        AI_Output(self, other, PATCH_AE_DIA_MATTEO_HUNTERJACKET_09_02);
+    };
+};
+
+//-------------------------------------
+var int Patch_AE_Matteo_HUNTERJACKETHBought;
+//-------------------------------------
+
+instance Patch_AE_DIA_Matteo_HUNTERJACKETH (C_INFO)
+{
+    npc          =  VLK_416_Matteo;
+    nr           =  852;
+    condition    =  Patch_AE_DIA_Matteo_HUNTERJACKETH_Condition;
+    information  =  Patch_AE_DIA_Matteo_HUNTERJACKETH_Info;
+    permanent    =  TRUE;
+    description  =  Patch_AE_DIA_Matteo_HUNTERJACKETH_desc;
+};
+func int Patch_AE_DIA_Matteo_HUNTERJACKETH_Condition()
+{
+    if (Npc_KnowsInfo(other, DIA_Matteo_SellWhat))
+    && (Patch_AE_Matteo_HUNTERJACKETHBought == FALSE)
+    && (Kapitel >= 2)
+    {
+        return TRUE;
+    };
+};
+func void Patch_AE_DIA_Matteo_HUNTERJACKETH_Info()
+{
+    AI_Output(other, self, PATCH_AE_DIA_MATTEO_HUNTERJACKETH_15_00);
+
+    if (B_GiveInvItems(other, self, ItMi_Gold, 1800))
+    {
+        AI_Output(self, other, PATCH_AE_DIA_MATTEO_HUNTERJACKETH_09_01);
+
+        Patch_AE_Matteo_HUNTERJACKETHBought = B_GiveInvItems(self, other, ItAr_HUNTERJACKETH_ARMOREXPANSION, 1);
+    }
+    else
+    {
+        AI_Output(self, other, PATCH_AE_DIA_MATTEO_HUNTERJACKETH_09_02);
+    };
+};
