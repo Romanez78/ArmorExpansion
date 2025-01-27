fc3f648068b7b4b105a1db0d2c5178306da79e29
Romanez78 <85283033+Romanez78@users.noreply.github.com>
2025-01-27 19:29:31 +0100

    Update Wolf.d
    The dialogue does not remove the light mercenary armor from the players inventory, which is a bug.

--- Ninja/ArmorExpansion/Content/Dialogs/Wolf.d
+++ Ninja/ArmorExpansion/Content/Dialogs/Wolf.d
@@ -20,6 +20,7 @@ func void Patch_AE_DIA_WOLF_SLD_SCOUTArmorBought_Info()
     AI_Output(other, self, PATCH_AE_DIA_WOLF_SLD_SCOUTARMORBOUGHT_15_01);
     AI_Output(other, self, PATCH_AE_DIA_WOLF_SLD_SCOUTARMORBOUGHT_15_02);
     AI_Output(self, other, PATCH_AE_DIA_WOLF_SLD_SCOUTARMORBOUGHT_13_03);
+    B_GiveInvItems(other, self, ItAr_SLD_L, 1);
     B_GiveInvItems(self, other, ItAr_SLD_SCOUT_ARMOREXPANSION, 1);
     AI_Output(other, self, PATCH_AE_DIA_WOLF_SLD_SCOUTARMORBOUGHT_15_06);
     AI_Output(self, other, PATCH_AE_DIA_WOLF_SLD_SCOUTARMORBOUGHT_13_07);
