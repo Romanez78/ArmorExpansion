70aa30402b71c4fdace12316aaa415606dae2468
Romanez78 <85283033+Romanez78@users.noreply.github.com>
2025-02-19 21:53:19 +0100

    PAL_CH Dialogue Hagen.d
    Hagen will give out this armor instead of the dialogue with Harad

--- Ninja/ArmorExpansion/Content/Dialogs/Hagen.d
+++ Ninja/ArmorExpansion/Content/Dialogs/Hagen.d
@@ -24,3 +24,30 @@ func void Patch_AE_DIA_Lord_Hagen_CROSSBOW_Info()
     B_GiveInvItems(self, other, ITAR_PAL_C_ARMOREXPANSION, 1);
     AI_Output(self, other, PATCH_AE_DIA_LORD_HAGEN_CROSSBOW_15_9);
 };
+
+instance Patch_AE_DIA_Lord_Hagen_HEAVYCROSSBOW (C_INFO)
+{
+    npc          =  PAL_200_Hagen;
+    nr           =  990;
+    condition    =  Patch_AE_DIA_Lord_Hagen_HEAVYCROSSBOW_Condition;
+    information  =  Patch_AE_DIA_Lord_Hagen_HEAVYCROSSBOW_Info;
+    permanent    =  FALSE;
+    description  =  Patch_AE_DIA_Lord_Hagen_HEAVYCROSSBOW_desc;
+};
+func int Patch_AE_DIA_Lord_Hagen_HEAVYCROSSBOW_Condition()
+{
+    if ((ItWr_SCReadsHallsofIrdorath == TRUE) && Npc_KnowsInfo(other,DIA_Lord_Hagen_AllDragonsDead))
+    {
+        return TRUE;
+    };
+};
+func void Patch_AE_DIA_Lord_Hagen_HEAVYCROSSBOW_Info()
+{
+    AI_Output(other, self, PATCH_AE_DIA_LORD_HAGEN_HEAVYCROSSBOW_15_01);
+    AI_Output(self, other, PATCH_AE_DIA_LORD_HAGEN_HEAVYCROSSBOW_13_03);
+    AI_Output(self, other, PATCH_AE_DIA_LORD_HAGEN_HEAVYCROSSBOW_13_04);    
+        B_GiveInvItems(self, other, ITAR_PAL_CH_ARMOREXPANSION, 1);
+    AI_Output(other, self, PATCH_AE_DIA_LORD_HAGEN_HEAVYCROSSBOW_15_05);
+    AI_Output(self, other, PATCH_AE_DIA_LORD_HAGEN_HEAVYCROSSBOW_15_06);
+};
+
