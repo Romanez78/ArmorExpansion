a8a112462218b84dce1ccec2a49c2c1903db40d8
Romanez78 <85283033+Romanez78@users.noreply.github.com>
2025-02-21 00:07:39 +0100

    Updated IT_Belts.d
    reverted change to leather belt script, since there was no reason for what i was doing

--- Ninja/ArmorExpansion/Content/Items/IT_Belts.d
+++ Ninja/ArmorExpansion/Content/Items/IT_Belts.d
@@ -68,14 +68,13 @@ func void Patch_AE_Equip_ItBE_Addon_Leather_01()
 {
     if (true == Npc_IsPlayer(self))
     {
-        if (false == Patch_AE__ITAR_GAMBESON_Equipped)
+        if (true == Patch_AE__ITAR_GAMBESON_Equipped)
         {
             self.protection[PROT_EDGE] += BA_Bonus01;
             self.protection[PROT_BLUNT] += BA_Bonus01;
             self.protection[PROT_POINT] += BA_Bonus01;
             self.protection[PROT_MAGIC] += BA_Bonus01;
             self.protection[PROT_FIRE] += BA_Bonus01;
-            Patch_AE__ITAR_GAMBESON_Equipped = true;
         };
     };
 
@@ -92,7 +91,6 @@ func void Patch_AE_UnEquip_ItBE_Addon_Leather_01()
             self.protection[PROT_POINT] -= BA_Bonus01;
             self.protection[PROT_MAGIC] -= BA_Bonus01;
             self.protection[PROT_FIRE] -= BA_Bonus01;
-            Patch_AE__ITAR_GAMBESON_Equipped = false;
         };
     };
 
@@ -103,14 +101,13 @@ func void Patch_AE_Equip_ItBE_Addon_MC()
 {
     if (true == Npc_IsPlayer(self))
     {
-        if (false == Patch_AE__ITAR_GAMBESON_Equipped)
+        if (true == Patch_AE__ITAR_GAMBESON_Equipped)
         {
             self.protection[PROT_EDGE] += BA_Bonus01;
             self.protection[PROT_BLUNT] += BA_Bonus01;
             self.protection[PROT_POINT] += BA_Bonus01;
             self.protection[PROT_MAGIC] += BA_Bonus01;
             self.protection[PROT_FIRE] += BA_Bonus01;
-             Patch_AE__ITAR_GAMBESON_Equipped = true;
         };
     };
 
@@ -127,7 +124,6 @@ func void Patch_AE_UnEquip_ItBE_Addon_MC()
             self.protection[PROT_POINT] -= BA_Bonus01;
             self.protection[PROT_MAGIC] -= BA_Bonus01;
             self.protection[PROT_FIRE] -= BA_Bonus01;
-             Patch_AE__ITAR_GAMBESON_Equipped = false;
         };
     };
 
