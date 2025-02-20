6c645a59a617f3a518c30f38ce5321b8c27d2b05
Romanez78 <85283033+Romanez78@users.noreply.github.com>
2025-02-20 23:58:13 +0100

    Updated IT_Belts.d
    Another attempt at making belts work, now with crawler armor belt.

--- Ninja/ArmorExpansion/Content/Items/IT_Belts.d
+++ Ninja/ArmorExpansion/Content/Items/IT_Belts.d
@@ -68,13 +68,14 @@ func void Patch_AE_Equip_ItBE_Addon_Leather_01()
 {
     if (true == Npc_IsPlayer(self))
     {
-        if (true == Patch_AE__ITAR_GAMBESON_Equipped)
+        if (false == Patch_AE__ITAR_GAMBESON_Equipped)
         {
             self.protection[PROT_EDGE] += BA_Bonus01;
             self.protection[PROT_BLUNT] += BA_Bonus01;
             self.protection[PROT_POINT] += BA_Bonus01;
             self.protection[PROT_MAGIC] += BA_Bonus01;
             self.protection[PROT_FIRE] += BA_Bonus01;
+            Patch_AE__ITAR_GAMBESON_Equipped = true;
         };
     };
 
@@ -91,6 +92,42 @@ func void Patch_AE_UnEquip_ItBE_Addon_Leather_01()
             self.protection[PROT_POINT] -= BA_Bonus01;
             self.protection[PROT_MAGIC] -= BA_Bonus01;
             self.protection[PROT_FIRE] -= BA_Bonus01;
+            Patch_AE__ITAR_GAMBESON_Equipped = false;
+        };
+    };
+
+    ContinueCall();
+};
+
+func void Patch_AE_Equip_ItBE_Addon_MC()
+{
+    if (true == Npc_IsPlayer(self))
+    {
+        if (false == Patch_AE__ITAR_GAMBESON_Equipped)
+        {
+            self.protection[PROT_EDGE] += BA_Bonus01;
+            self.protection[PROT_BLUNT] += BA_Bonus01;
+            self.protection[PROT_POINT] += BA_Bonus01;
+            self.protection[PROT_MAGIC] += BA_Bonus01;
+            self.protection[PROT_FIRE] += BA_Bonus01;
+             Patch_AE__ITAR_GAMBESON_Equipped = true;
+        };
+    };
+
+    ContinueCall();
+};
+func void Patch_AE_UnEquip_ItBE_Addon_MC()
+{
+    if (true == Npc_IsPlayer(self))
+    {
+        if (true == Patch_AE__ITAR_GAMBESON_Equipped)
+        {
+            self.protection[PROT_EDGE] -= BA_Bonus01;
+            self.protection[PROT_BLUNT] -= BA_Bonus01;
+            self.protection[PROT_POINT] -= BA_Bonus01;
+            self.protection[PROT_MAGIC] -= BA_Bonus01;
+            self.protection[PROT_FIRE] -= BA_Bonus01;
+             Patch_AE__ITAR_GAMBESON_Equipped = false;
         };
     };
 
