ed969bbf6c651e3b4e63dbc3b001d853aa94a897
Romanez78 <85283033+Romanez78@users.noreply.github.com>
2026-05-06 15:38:52 +0200

    Update Harad.d
    Updated dialogue code to include helmet variations

--- Ninja/ArmorExpansion/Content/Dialogs/Harad.d
+++ Ninja/ArmorExpansion/Content/Dialogs/Harad.d
@@ -9,7 +9,7 @@ instance Patch_AE_DIA_HARAD_PAL_SHOW (C_INFO)
 };
 func int Patch_AE_DIA_HARAD_PAL_SHOW_Condition()
 {
-    if (Npc_Hasitems(other, ItAr_PAL_R_ARMOREXPANSION) > 0)
+    if (Patch_AE_Martin_ARKPALA_BOUGHT == TRUE)
     {
         return TRUE;
     };
@@ -19,12 +19,10 @@ func void Patch_AE_DIA_HARAD_PAL_SHOW_Info()
     B_GivePlayerXP(10);
     AI_Output(other, self, PATCH_AE_DIA_HARAD_PAL_SHOW_4_1);
     AI_Output(self, other, PATCH_AE_DIA_HARAD_PAL_SHOW_3_01);
-    B_GiveInvItems(other, self, ItAr_PAL_R_ARMOREXPANSION, 1);
     AI_Output(self, other, PATCH_AE_DIA_HARAD_PAL_SHOW_3_02);
     AI_Output(self, other, PATCH_AE_DIA_HARAD_PAL_SHOW_3_03);
     AI_Output(self, other, PATCH_AE_DIA_HARAD_PAL_SHOW_3_04);
     AI_Output(self, other, PATCH_AE_DIA_HARAD_PAL_SHOW_3_05);
-    B_GiveInvItems(self, other, ITAR_PAL_R_ARMOREXPANSION, 1);
     AI_Output(self, other, PATCH_AE_DIA_HARAD_PAL_SHOW_3_06);
     AI_Output(other, self, PATCH_AE_DIA_HARAD_PAL_SHOW_3_07);
     B_LogEntry(Patch_AE_TOPIC_MARTIN_ARMOR, Patch_AE_DIA_HARAD_PAL_SHOW_entry);
@@ -47,6 +45,7 @@ func int Patch_AE_DIA_HARAD_PALADIN_REFORGE_Condition()
 {
     if (Patch_AE_HARAD_PALADIN_REFORGEBought == FALSE)
     && (Npc_KnowsInfo (other, Patch_AE_DIA_HARAD_PAL_SHOW))
+    && (Npc_HasItems(other, ITAR_PAL_R_ArmorExpansion) > 0)
     {
         return TRUE;
     };
@@ -115,4 +114,167 @@ func void Patch_AE_DIA_HARAD_PALADIN_REFORGE_Info()
     };
 };
 
+instance Patch_AE_DIA_HARAD_PALADIN_REFORGE_FH (C_INFO)
+{
+    npc          =  VLK_412_Harad;
+    nr           =  8;
+    condition    =  Patch_AE_DIA_HARAD_PALADIN_REFORGE_FH_Condition;
+    information  =  Patch_AE_DIA_HARAD_PALADIN_REFORGE_FH_Info;
+    permanent    =  TRUE;
+    description  =  Patch_AE_DIA_HARAD_PALADIN_REFORGE_desc;
+};
+func int Patch_AE_DIA_HARAD_PALADIN_REFORGE_FH_Condition()
+{
+    if (Patch_AE_HARAD_PALADIN_REFORGEBought == FALSE)
+    && (Npc_KnowsInfo (other, Patch_AE_DIA_HARAD_PAL_SHOW))
+    && (Npc_HasItems(other, ITAR_PAL_RFH_ArmorExpansion) > 0)
+    {
+        return TRUE;
+    };
+};
+func void Patch_AE_DIA_HARAD_PALADIN_REFORGE_FH_Info()
+{
+    AI_Output(other, self, PATCH_AE_DIA_HARAD_PALADIN_REFORGE_15_01);
+    AI_Output(self, other, PATCH_AE_DIA_HARAD_PALADIN_REFORGE_13_02);
+    AI_Output(self, other, PATCH_AE_DIA_HARAD_PALADIN_REFORGE_04);
+    AI_Output(self, other, PATCH_AE_DIA_HARAD_PALADIN_REFORGE_13_05);
+    AI_Output(other, self, PATCH_AE_DIA_HARAD_PALADIN_REFORGE_13_06);
+    AI_Output(self, other, PATCH_AE_DIA_HARAD_PALADIN_REFORGE_13_07);
+    AI_Output(other, self, PATCH_AE_DIA_HARAD_PALADIN_REFORGE_15_08);
+    B_LogEntry(Patch_AE_TOPIC_MARTIN_ARMOR, Patch_AE_DIA_HARAD_PALADIN_REFORGE_entry1);
+
+    if (Npc_HasItems(other, ITAR_PAL_RFH_ArmorExpansion) > 0)
+    && (Npc_HasItems(other, Patch_AE_SmithItemCold) >= 6)
+    && (Npc_HasItems(other, ItMi_Nugget) >= 3)
+    {
+        B_GivePlayerXP(100);
+        B_GiveInvItems(other, self, ITAR_PAL_RFH_ArmorExpansion, 1);
+        AI_Output(other, self, PATCH_AE_DIA_HARAD_PALADIN_REFORGE_15_9);
+        B_GiveInvItems(other, self, Patch_AE_SmithItemCold, 6);
+        B_GiveInvItems(other, self, ItMi_Nugget, 2);
+        AI_Output(other, self, PATCH_AE_DIA_HARAD_PALADIN_REFORGE_15_10);
+        AI_Output(self, other, PATCH_AE_DIA_HARAD_PALADIN_REFORGE_13_11);
+
+        B_LookAtNpc(other, self);
+        AI_UseMob(self, "BSFIRE", 1);
+        AI_UseMob(self, "BSFIRE", -1);
+        B_TurnToNpc(other, self);
+
+        AI_UseMob(self, "BSANVIL", 1);
+        AI_UseMob(self, "BSANVIL", -1);
+        B_TurnToNpc(other, self);
+
+        AI_UseMob(self, "BSCOOL", 1);
+        AI_UseMob(self, "BSCOOL", -1);
+        B_TurnToNpc(other, self);
+
+        B_LookAtNpc(other, self);
+        AI_UseMob(self, "BSFIRE", 1);
+        AI_UseMob(self, "BSFIRE", -1);
+        B_TurnToNpc(other, self);
+
+        AI_UseMob(self, "BSANVIL", 1);
+        AI_UseMob(self, "BSANVIL", -1);
+        B_TurnToNpc(other, self);
+
+        AI_UseMob(self, "BSCOOL", 1);
+        AI_UseMob(self, "BSCOOL", -1);
+        B_TurnToNpc(other, self);
+        B_TurnToNpc(self, other);
+
+        AI_Output(self, other, PATCH_AE_DIA_HARAD_PALADIN_REFORGE_13_12);
+        Patch_AE_HARAD_PALADIN_REFORGEBought = B_GiveInvItems(self, other, ITAR_PAL_FFH_ARMOREXPANSION, 1);
+        AI_Output(self, other, PATCH_AE_DIA_HARAD_PALADIN_REFORGE_13_13);
+        AI_Output(self, other, PATCH_AE_DIA_HARAD_PALADIN_REFORGE_13_14);
+        AI_Output(self, other, PATCH_AE_DIA_HARAD_PALADIN_REFORGE_13_15);
+        AI_Output(other, self, PATCH_AE_DIA_HARAD_PALADIN_REFORGE_15_14);
+        B_LogEntry(Patch_AE_TOPIC_MARTIN_ARMOR, Patch_AE_DIA_HARAD_PALADIN_REFORGE_entry2);
+    }
+    else
+    {
+        AI_Output(self, other, PATCH_AE_DIA_HARAD_PALADIN_REFORGE_13_16);
+    };
+};
+
+instance Patch_AE_DIA_HARAD_PALADIN_REFORGE_NH (C_INFO)
+{
+    npc          =  VLK_412_Harad;
+    nr           =  8;
+    condition    =  Patch_AE_DIA_HARAD_PALADIN_REFORGE_NH_Condition;
+    information  =  Patch_AE_DIA_HARAD_PALADIN_REFORGE_NH_Info;
+    permanent    =  TRUE;
+    description  =  Patch_AE_DIA_HARAD_PALADIN_REFORGE_desc;
+};
+func int Patch_AE_DIA_HARAD_PALADIN_REFORGE_NH_Condition()
+{
+    if (Patch_AE_HARAD_PALADIN_REFORGEBought == FALSE)
+    && (Npc_KnowsInfo (other, Patch_AE_DIA_HARAD_PAL_SHOW))
+    && (Npc_HasItems(other, ITAR_PAL_RNH_ArmorExpansion) > 0)     
+    {
+        return TRUE;
+    };
+};
+func void Patch_AE_DIA_HARAD_PALADIN_REFORGE_NH_Info()
+{
+    AI_Output(other, self, PATCH_AE_DIA_HARAD_PALADIN_REFORGE_15_01);
+    AI_Output(self, other, PATCH_AE_DIA_HARAD_PALADIN_REFORGE_13_02);
+    AI_Output(self, other, PATCH_AE_DIA_HARAD_PALADIN_REFORGE_04);
+    AI_Output(self, other, PATCH_AE_DIA_HARAD_PALADIN_REFORGE_13_05);
+    AI_Output(other, self, PATCH_AE_DIA_HARAD_PALADIN_REFORGE_13_06);
+    AI_Output(self, other, PATCH_AE_DIA_HARAD_PALADIN_REFORGE_13_07);
+    AI_Output(other, self, PATCH_AE_DIA_HARAD_PALADIN_REFORGE_15_08);
+    B_LogEntry(Patch_AE_TOPIC_MARTIN_ARMOR, Patch_AE_DIA_HARAD_PALADIN_REFORGE_entry1);
+
+    if (Npc_HasItems(other, ITAR_PAL_RNH_ArmorExpansion) > 0)
+    && (Npc_HasItems(other, Patch_AE_SmithItemCold) >= 6)
+    && (Npc_HasItems(other, ItMi_Nugget) >= 3)
+    {
+        B_GivePlayerXP(100);
+        B_GiveInvItems(other, self, ITAR_PAL_RNH_ArmorExpansion, 1);
+        AI_Output(other, self, PATCH_AE_DIA_HARAD_PALADIN_REFORGE_15_9);
+        B_GiveInvItems(other, self, Patch_AE_SmithItemCold, 6);
+        B_GiveInvItems(other, self, ItMi_Nugget, 2);
+        AI_Output(other, self, PATCH_AE_DIA_HARAD_PALADIN_REFORGE_15_10);
+        AI_Output(self, other, PATCH_AE_DIA_HARAD_PALADIN_REFORGE_13_11);
+
+        B_LookAtNpc(other, self);
+        AI_UseMob(self, "BSFIRE", 1);
+        AI_UseMob(self, "BSFIRE", -1);
+        B_TurnToNpc(other, self);
+
+        AI_UseMob(self, "BSANVIL", 1);
+        AI_UseMob(self, "BSANVIL", -1);
+        B_TurnToNpc(other, self);
+
+        AI_UseMob(self, "BSCOOL", 1);
+        AI_UseMob(self, "BSCOOL", -1);
+        B_TurnToNpc(other, self);
+
+        B_LookAtNpc(other, self);
+        AI_UseMob(self, "BSFIRE", 1);
+        AI_UseMob(self, "BSFIRE", -1);
+        B_TurnToNpc(other, self);
+
+        AI_UseMob(self, "BSANVIL", 1);
+        AI_UseMob(self, "BSANVIL", -1);
+        B_TurnToNpc(other, self);
+
+        AI_UseMob(self, "BSCOOL", 1);
+        AI_UseMob(self, "BSCOOL", -1);
+        B_TurnToNpc(other, self);
+        B_TurnToNpc(self, other);
+
+        AI_Output(self, other, PATCH_AE_DIA_HARAD_PALADIN_REFORGE_13_12);
+        Patch_AE_HARAD_PALADIN_REFORGEBought = B_GiveInvItems(self, other, ITAR_PAL_FNH_ARMOREXPANSION, 1);
+        AI_Output(self, other, PATCH_AE_DIA_HARAD_PALADIN_REFORGE_13_13);
+        AI_Output(self, other, PATCH_AE_DIA_HARAD_PALADIN_REFORGE_13_14);
+        AI_Output(self, other, PATCH_AE_DIA_HARAD_PALADIN_REFORGE_13_15);
+        AI_Output(other, self, PATCH_AE_DIA_HARAD_PALADIN_REFORGE_15_14);
+        B_LogEntry(Patch_AE_TOPIC_MARTIN_ARMOR, Patch_AE_DIA_HARAD_PALADIN_REFORGE_entry2);
+    }
+    else
+    {
+        AI_Output(self, other, PATCH_AE_DIA_HARAD_PALADIN_REFORGE_13_16);
+    };
+};
 
