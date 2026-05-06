bcbeaab28105e4baa50d812d103328d7fd0112b8
Romanez78 <85283033+Romanez78@users.noreply.github.com>
2026-05-06 16:16:49 +0200

    Update Jan.d

--- Ninja/ArmorExpansion/Content/Dialogs/Jan.d
+++ Ninja/ArmorExpansion/Content/Dialogs/Jan.d
@@ -9,7 +9,9 @@ instance Patch_AE_DIA_JAN_PAL_REFORGE (C_INFO)
 };
 func int Patch_AE_DIA_JAN_PAL_REFORGE_Condition()
 {
-    if (Npc_Hasitems(other, ItAr_PAL_R_ARMOREXPANSION) > 0)
+    if (Npc_Hasitems(other, ItAr_PAL_R_ARMOREXPANSION) > 0))
+    || (Npc_HasItems (other, ItAr_PAL_RFH_ARMOREXPANSION) > 0))
+    || (Npc_HasItems (other, ItAr_PAL_RNH_ARMOREXPANSION) > 0))
     {
         return TRUE;
     };
@@ -22,38 +24,4 @@ func void Patch_AE_DIA_JAN_PAL_REFORGE_Info()
     AI_Output(self, other, PATCH_AE_DIA_JAN_PAL_REFORGE_3_02);
     AI_Output(self, other, PATCH_AE_DIA_JAN_PAL_REFORGE_3_03);
     B_LogEntry (Patch_AE_TOPIC_MARTIN_ARMOR, Patch_AE_DIA_JAN_PAL_REFORGE_entry);
-};
-
-instance Patch_AE_DIA_JAN_PALNH_REFORGE (C_INFO)
-{
-    npc          =  DJG_714_Jan;
-    nr           =  3;
-    condition    =  Patch_AE_DIA_JAN_PALNH_REFORGE_Condition;
-    information  =  Patch_AE_DIA_JAN_PAL_REFORGE_Info;
-    permanent    =  FALSE;
-    description  =  Patch_AE_DIA_JAN_PAL_REFORGE_desc;
-};
-func int Patch_AE_DIA_JAN_PALNH_REFORGE_Condition()
-{
-    if (Npc_Hasitems(other, ItAr_PAL_RNH_ARMOREXPANSION) > 0)
-    {
-        return TRUE;
-    };
-};
-
-instance Patch_AE_DIA_JAN_PALFH_REFORGE (C_INFO)
-{
-    npc          =  DJG_714_Jan;
-    nr           =  3;
-    condition    =  Patch_AE_DIA_JAN_PALFH_REFORGE_Condition;
-    information  =  Patch_AE_DIA_JAN_PAL_REFORGE_Info;
-    permanent    =  FALSE;
-    description  =  Patch_AE_DIA_JAN_PAL_REFORGE_desc;
-};
-func int Patch_AE_DIA_JAN_PALFH_REFORGE_Condition()
-{
-    if (Npc_Hasitems(other, ItAr_PAL_RFH_ARMOREXPANSION) > 0)
-    {
-        return TRUE;
-    };
 };
