e367f9b565c572c2affa20eaa22590be5d3b8ee9
Romanez78 <85283033+Romanez78@users.noreply.github.com>
2026-05-06 16:16:31 +0200

    Update Huno.d

--- Ninja/ArmorExpansion/Content/Dialogs/Huno.d
+++ Ninja/ArmorExpansion/Content/Dialogs/Huno.d
@@ -9,7 +9,9 @@ instance Patch_AE_DIA_HUNO_PAL_REFORGE (C_INFO)
 };
 func int Patch_AE_DIA_HUNO_PAL_REFORGE_Condition()
 {
-    if (Npc_Hasitems(other, ItAr_PAL_R_ARMOREXPANSION) > 0)
+    if (Npc_Hasitems(other, ItAr_PAL_R_ARMOREXPANSION) > 0))
+    || (Npc_HasItems (other, ItAr_PAL_RFH_ARMOREXPANSION) > 0))
+    || (Npc_HasItems (other, ItAr_PAL_RNH_ARMOREXPANSION) > 0))
     {
         return TRUE;
     };
@@ -22,39 +24,5 @@ func void Patch_AE_DIA_HUNO_PAL_REFORGE_Info()
     AI_Output(self, other, PATCH_AE_DIA_HUNO_PAL_REFORGE_3_02);
     AI_Output(self, other, PATCH_AE_DIA_HUNO_PAL_REFORGE_3_03);
     B_LogEntry (Patch_AE_TOPIC_MARTIN_ARMOR, Patch_AE_DIA_HUNO_PAL_REFORGE_entry);
-};
-
-instance Patch_AE_DIA_HUNO_PALNH_REFORGE (C_INFO)
-{
-    npc          =  BDT_1099_Addon_Huno;
-    nr           =  3;
-    condition    =  Patch_AE_DIA_HUNO_PALNH_REFORGE_Condition;
-    information  =  Patch_AE_DIA_HUNO_PAL_REFORGE_Info;
-    permanent    =  FALSE;
-    description  =  Patch_AE_DIA_HUNO_PAL_REFORGE_desc;
-};
-func int Patch_AE_DIA_HUNO_PALNH_REFORGE_Condition()
-{
-    if (Npc_Hasitems(other, ItAr_PAL_RNH_ARMOREXPANSION) > 0)
-    {
-        return TRUE;
-    };
-};
-
-instance Patch_AE_DIA_HUNO_PALFH_REFORGE (C_INFO)
-{
-    npc          =  BDT_1099_Addon_Huno;
-    nr           =  3;
-    condition    =  Patch_AE_DIA_HUNO_PALFH_REFORGE_Condition;
-    information  =  Patch_AE_DIA_HUNO_PAL_REFORGE_Info;
-    permanent    =  FALSE;
-    description  =  Patch_AE_DIA_HUNO_PAL_REFORGE_desc;
-};
-func int Patch_AE_DIA_HUNO_PALFH_REFORGE_Condition()
-{
-    if (Npc_Hasitems(other, ItAr_PAL_RFH_ARMOREXPANSION) > 0)
-    {
-        return TRUE;
-    };
 };
 
