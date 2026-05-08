d8c3bae52afd95093737969785988d794a904619
Romanez78 <85283033+Romanez78@users.noreply.github.com>
2026-05-08 08:01:02 +0200

    Update IT_ArmorExpansion.d

--- Ninja/ArmorExpansion/Content/Items/IT_ArmorExpansion.d
+++ Ninja/ArmorExpansion/Content/Items/IT_ArmorExpansion.d
@@ -1712,11 +1712,11 @@ INSTANCE ITAR_PAL_ST_NH_ArmorExpansion (C_Item)
 	mainflag 				=	ITEM_KAT_ARMOR;
 	flags 					=	0;
 
-	protection [PROT_EDGE]	=	ITAR_PAL_ST_ArmorExpansion__PROT_EDGE;
-	protection [PROT_BLUNT] = 	ITAR_PAL_ST_ArmorExpansion__PROT_BLUNT;
-	protection [PROT_POINT] = 	ITAR_PAL_ST_ArmorExpansion__PROT_POINT;
-	protection [PROT_FIRE] 	= 	ITAR_PAL_ST_ArmorExpansion__PROT_FIRE;
-	protection [PROT_MAGIC] = 	ITAR_PAL_ST_ArmorExpansion__PROT_MAGIC;
+	protection [PROT_EDGE]	=	ITAR_PAL_ST_NH_ArmorExpansion__PROT_EDGE;
+	protection [PROT_BLUNT] = 	ITAR_PAL_ST_NH_ArmorExpansion__PROT_BLUNT;
+	protection [PROT_POINT] = 	ITAR_PAL_ST_NH_ArmorExpansion__PROT_POINT;
+	protection [PROT_FIRE] 	= 	ITAR_PAL_ST_NH_ArmorExpansion__PROT_FIRE;
+	protection [PROT_MAGIC] = 	ITAR_PAL_ST_NH_ArmorExpansion__PROT_MAGIC;
 
 	value 					=	3000;
 
@@ -1773,11 +1773,11 @@ INSTANCE ITAR_PAL_ST_AE_ArmorExpansion (C_Item)
 	mainflag 				=	ITEM_KAT_ARMOR;
 	flags 					=	0;
 
-	protection [PROT_EDGE]	=	ITAR_PAL_ST_FH_ArmorExpansion__PROT_EDGE;
-	protection [PROT_BLUNT] = 	ITAR_PAL_ST_FH_ArmorExpansion__PROT_BLUNT;
-	protection [PROT_POINT] = 	ITAR_PAL_ST_FH_ArmorExpansion__PROT_POINT;
-	protection [PROT_FIRE] 	= 	ITAR_PAL_ST_FH_ArmorExpansion__PROT_FIRE;
-	protection [PROT_MAGIC] = 	ITAR_PAL_ST_FH_ArmorExpansion__PROT_MAGIC;
+	protection [PROT_EDGE]	=	ITAR_PAL_ST_ArmorExpansion__PROT_EDGE;
+	protection [PROT_BLUNT] = 	ITAR_PAL_ST_ArmorExpansion__PROT_BLUNT;
+	protection [PROT_POINT] = 	ITAR_PAL_ST_ArmorExpansion__PROT_POINT;
+	protection [PROT_FIRE] 	= 	ITAR_PAL_ST_ArmorExpansion__PROT_FIRE;
+	protection [PROT_MAGIC] = 	ITAR_PAL_ST_ArmorExpansion__PROT_MAGIC;
 
 	value 					=	3000;
 
@@ -1816,11 +1816,11 @@ INSTANCE ITAR_PAL_ST_FH_ArmorExpansion (C_Item)
 	mainflag 				=	ITEM_KAT_ARMOR;
 	flags 					=	0;
 
-	protection [PROT_EDGE]	=	ITAR_PAL_ST_FH_ArmorExpansion__PROT_EDGE;
-	protection [PROT_BLUNT] = 	ITAR_PAL_ST_FH_ArmorExpansion__PROT_BLUNT;
-	protection [PROT_POINT] = 	ITAR_PAL_ST_FH_ArmorExpansion__PROT_POINT;
-	protection [PROT_FIRE] 	= 	ITAR_PAL_ST_FH_ArmorExpansion__PROT_FIRE;
-	protection [PROT_MAGIC] = 	ITAR_PAL_ST_FH_ArmorExpansion__PROT_MAGIC;
+	protection [PROT_EDGE]	=	ITAR_PAL_ST_ArmorExpansion__PROT_EDGE;
+	protection [PROT_BLUNT] = 	ITAR_PAL_ST_ArmorExpansion__PROT_BLUNT;
+	protection [PROT_POINT] = 	ITAR_PAL_ST_ArmorExpansion__PROT_POINT;
+	protection [PROT_FIRE] 	= 	ITAR_PAL_ST_ArmorExpansion__PROT_FIRE;
+	protection [PROT_MAGIC] = 	ITAR_PAL_ST_ArmorExpansion__PROT_MAGIC;
 
 	value 					=	3000;
 
